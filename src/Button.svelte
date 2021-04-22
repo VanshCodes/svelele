4324dbf356b4994f223b6407ed054c9c992b70b1
Vansh <66193724+VanshCodes@users.noreply.github.com>
2021-04-22 16:30:24 +0530

    Sv askkkka

--- src/Button.svelte
+++ src/Button.svelte
@@ -7,15 +7,24 @@
 </script>
 
 <style>
+	#filers-css-generator-demo {
+filter: brightness(85%) saturate(484%) contrast(86%) drop-shadow(81px 162px 94px #332c2c);
+-webkit-filter: brightness(85%) saturate(484%) contrast(86%) drop-shadow(81px 162px 94px #332c2c);
+-moz-filter: brightness(85%) saturate(484%) contrast(86%) drop-shadow(81px 162px 94px #332c2c);
+	}
 	button {
 	  background: #ff3e00;
 	  color: white;
-	  border: none;
+	  border: 15px solid #1c6ea4;
 	  padding: 8px 12px;
 	  border-radius: 2px;
 	}
 </style>
-
+<div id="filer-css-generator-demo" style="">
 <button on:click={handleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+        </div>
+<div id="filers-css-generator-demo">
+          <img src="https://cssgenerator.org/images/img_for_filter.jpg" alt="Demo Image for Filter CSS Effects">
+        </div>
