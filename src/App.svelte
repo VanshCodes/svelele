10ed269f46cacf38275e50d8c90dd9283246ffae
Vansh <66193724+VanshCodes@users.noreply.github.com>
2021-04-22 21:02:40 +0530

    Update Buttom

--- src/App.svelte
+++ src/App.svelte
@@ -7,14 +7,16 @@
     font-family: sans-serif;
     text-align: center;
   }
+h1{
+color: cornsilk
+}
+h2{
+color: #59F8E8;
+}
 </style>
 
 <main>
 	<h1>Hello CodeSandbox</h1>
-	<h2>Start editing to see some magic happen!</h2>
-	<h2>Start editing to see some magic happen!</h2>
-	<h2>Start editing to see some magic happen!</h2>
-	<h2>Start editing to see some magic happen!</h2>
 	<h2>Start editing to see some magic happen!</h2>
 	<Button />
 </main>
--- src/Button.svelte
+++ src/Button.svelte
@@ -7,24 +7,62 @@
 </script>
 
 <style>
-	#filers-css-generator-demo {
-filter: brightness(85%) saturate(484%) contrast(86%) drop-shadow(81px 162px 94px #332c2c);
--webkit-filter: brightness(85%) saturate(484%) contrast(86%) drop-shadow(81px 162px 94px #332c2c);
--moz-filter: brightness(85%) saturate(484%) contrast(86%) drop-shadow(81px 162px 94px #332c2c);
-	}
-	button {
-	  background: #ff3e00;
-	  color: white;
-	  border: 15px solid #1c6ea4;
-	  padding: 8px 12px;
-	  border-radius: 2px;
-	}
+          button {
+            color: rgb(0, 0, 0);
+            font-size: 19px;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+              Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+            border: none;
+
+            /* transition-delay: 100ms; */
+            padding: 8px 12px;
+            background: #000000;
+            /* background-image: -webkit-linear-gradient(top, #000000, #b82b2b);
+                      background-image: -moz-linear-gradient(top, #000000, #b82b2b);
+                      background-image: -ms-linear-gradient(top, #000000, #b82b2b);
+                      background-image: -o-linear-gradient(top, #000000, #b82b2b);
+                      background-image: linear-gradient(to bottom, #000000, #b82b2b); */
+            -webkit-border-radius: 28;
+            -moz-border-radius: 28;
+            border-radius: 8px;
+            font-family: Arial;
+            color: #ffffff;
+            font-size: 20px;
+            padding: 10px 20px 10px 20px;
+            text-decoration: none;
+            transition: all 0.5s linear;
+            -moz-transition: all 0.5s linear; /* Firefox 4 */
+            -webkit-transition: all 0.5s linear; /* Safari and Chrome */
+            -o-transition: all 0.5s linear; /* Opera */
+            -ms-transition: all 0.5s linear; /* Explorer 10 */
+            /* transition: all 100ms linear 10s; */
+            /* transition-property: all;
+                              transition-duration: 10s;
+                              transition-timing-function: linear; */
+          }
+          button:hover {
+            background: #fe5f00;
+
+            /* filter: brightness(80%); */
+            /* background: #e016e0;
+                            background-image: -webkit-linear-gradient(top, #e016e0, #2c3fb8);
+                            background-image: -moz-linear-gradient(top, #e016e0, #2c3fb8);
+                            background-image: -ms-linear-gradient(top, #e016e0, #2c3fb8);
+                            background-image: -o-linear-gradient(top, #e016e0, #2c3fb8);
+                            background-image: linear-gradient(to bottom, #e016e0, #2c3fb8); */
+            /* -webkit-border-radius: 28;
+                            -moz-border-radius: 28; */
+            /* border-radius: 28px;
+                            font-family: Arial;
+                            color: #ffffff;
+                            font-size: 20px;
+                            padding: 10px 20px 10px 20px;
+                            text-decoration: none; */
+
+            /* width: 50%; */
+          }
 </style>
-<div id="filer-css-generator-demo" style="">
+
 <button on:click={handleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
-        </div>
-<div id="filers-css-generator-demo">
-          <img src="https://cssgenerator.org/images/img_for_filter.jpg" alt="Demo Image for Filter CSS Effects">
-        </div>
